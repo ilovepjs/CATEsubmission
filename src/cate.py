cbb20c339172aa9a42bb3e2c2195ba632582a086
ilovepjs <server@fake.co>
2013-12-06 00:27:16 +0000

    added tests, refactored code

--- src/cate.py
+++ src/cate.py
@@ -9,12 +9,12 @@ from requests.auth import HTTPBasicAuth
 
 class CateSubmission:
     def __init__(self, username, password, file_path):
-        self.baseURL = 'https://cate.doc.ic.ac.uk/'
+        self.base_url = 'https://cate.doc.ic.ac.uk/'
         self.auth = HTTPBasicAuth(username, password)
         self.file_path = file_path
 
     def fetch_cate_homepage(self):
-        r = requests.get(self.baseURL, auth=self.auth)
+        r = requests.get(self.base_url, auth=self.auth)
         return BeautifulSoup(r.text)
 
     # extracts timetable auth from cate page 
@@ -33,7 +33,7 @@ class CateSubmission:
         'period':timetable_period
         }
         
-        r = requests.get(self.baseURL + 'timetable.cgi', auth=self.auth, params=payload)
+        r = requests.get(self.base_url + 'timetable.cgi', auth=self.auth, params=payload)
         return BeautifulSoup(r.text)
 
     # gets timetable page with assignments on it
@@ -55,24 +55,27 @@ class CateSubmission:
         for i, key in enumerate(handin_urls.keys()):
             print '[{}]\t{}'.format(i, key)
 
-        invalid_key_chosen = True
+        invalid_key_selected = True
         selected_key = None
-        while invalid_key_chosen:
-            selected_key = raw_input('Select the number of the assignment to submit: ')
-            invalid_key_chosen = (selected_key > handin_urls.keys() or selected_key < 0)
+        while invalid_key_selected:
+            selected_key = int(raw_input('Select the number of the assignment to submit: '))
+            invalid_key_selected = selected_key > len(handin_urls) or selected_key < 0
 
         return handin_urls[handin_urls.keys()[int(selected_key)]]
 
+    def fetch_submission_page(submission_url):
+        r = requests.get(self.base_url + submission_url, auth=self.auth)
+        return r.text
+    
     # gets the assignment hand in page
     # submits the assigment (group or solo)
-    def submit_assignment(self, submission_url):
-        r = requests.get(submissionURL, auth=auth)
-        if ('Group' in r.text):
-            submit_group_assignment(submissionURL)
+    def submit_assignment(self, submission_page):
+        if ('Group' in submission_page):
+            submit_group_assignment(submission_url)
         else:
-            submit_page = _submit_declaration(baseURL, auth)
+            submit_page = _submit_declaration(base_url, auth)
             files = _get_file()
-            _submit_file(submissionURL, files, auth, submit_page)
+            _submit_file(submission_url, files, auth, submit_page)
 
     # checks submission page to see if decleration exists
     # if it does, signs it else creates one 
@@ -81,12 +84,12 @@ class CateSubmission:
             print 'Do you want to create a group on CATE and submit the work'
             user_response = raw_input('Do you want to create one? [Y/n]: ')
             if ('Y' in user_response):
-                r = _submit_declaration(baseURL, auth)
+                r = _submit_declaration(base_url, auth)
                 soup = BeautifulSoup(r.text)
 
                 _add_members_to_group()
                 files = _get_file()
-                _submit_file(submissionURL, files, auth)
+                _submit_file(submission_url, files, auth)
             else:
                 print 'Come back to sign the declaration once a group has been formed'
                 exit()
@@ -96,7 +99,7 @@ class CateSubmission:
             'key':_get_value_by_name(soup, 'key', {'type':'hidden'}),
             user_details['name']:user_details['value']
             }
-            requests.post(submissionURL, data=payload, auth=auth)
+            requests.post(submission_url, data=payload, auth=auth)
             print 'Your declaration has been submitted'
             exit()
 
@@ -114,7 +117,7 @@ class CateSubmission:
                     validation = raw_input('Is {} correct? [Y/n]: '.format(user_id_value))
                     if ('Y' in validation):
                         payload = { 'grpmember':user_id_value, 'key':add_grpmember_key}
-                        r = requests.post(submissionURL, data=payload, auth=auth)
+                        r = requests.post(submission_url, data=payload, auth=auth)
                         print '{} has been added to group'.format(user_id_value)
                     else:
                         print ('Invalid user id, please try again, E.G hj1612')
@@ -145,11 +148,11 @@ class CateSubmission:
             print 'Fatal: Not a git repository (or any of the parent directories): .git'
             exit()
 
-    def _submit_file(self, submissionURL, files, auth):
+    def _submit_file(self, submission_url, files, auth):
         submit_key = _get_value_by_name('key').split(':')
         submit_key = ':'.join(submit_key[:4] + ['submit',] + submit_key[5:])
         payload={'key':submit_key}
-        r = requests.post(submissionURL, files=files, data=payload, auth=auth)
+        r = requests.post(submission_url, files=files, data=payload, auth=auth)
 
         if 'NOT SUBMITTED' in r.text:
             print 'File failed to upload, check extension or base name'
@@ -159,16 +162,16 @@ class CateSubmission:
             print 'Something went wrong, try again or submit on CATE'
             exit()
 
-    def _submit_declaration(self, baseURL, auth):
+    def _submit_declaration(self, base_url, auth):
         payload = { 
         'inLeader':_get_value_by_name('inLeader'), 
         'inMember':_get_value_by_name('inMember'), 
         'version':_get_value_by_name('version'),
         'key':_get_value_by_name('key')
         }
-        declartionURL = soup.find('form')['action']
+        declartion_url = soup.find('form')['action']
 
-        return requests.post(baseURL + declartionURL, data=payload, auth=auth)
+        return requests.post(base_url + declartion_url, data=payload, auth=auth)
 
     def _get_value_by_name(self, soup, name, extra_attrs={}):
         attrs = {'name':name}
@@ -188,8 +191,9 @@ def main():
 
     timetable = cate.fetch_timetable(timetable_key, timetable_class, timetable_period)
     assignments = cate.get_assignments(timetable)
-    
-    cate.get_submission_url(assignments)
+
+    submission_url = cate.get_submission_url(assignments)
+    submission_page = cate.fetch_submission_page(submission_url)
 
 
     # def get_assignments(self, timetable_key, timetable_class, timetable_period):
