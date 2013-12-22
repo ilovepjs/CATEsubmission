c6947aee6cad79be1e43626114297fcfaf8a8554
ilovepjs <server@fake.co>
2013-12-22 16:12:25 +0000

    fixed file upload for multiple files, refactored code, fixed some bugs

--- src/cate.py
+++ src/cate.py
@@ -1,14 +1,15 @@
 import requests
-import os 
-import readline
+import os
 import getpass
 import subprocess
 import re
 import sys
 import readline
+import collections
 
 from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
+
 
 class CateSubmission:
     def __init__(self, username_input, password_input, file_path=None):
@@ -28,7 +29,7 @@ class CateSubmission:
         r = requests.get(self.base_url, auth=self.auth)
         return BeautifulSoup(r.text)
 
-    # extracts timetable auth from cate page 
+    # extracts timetable auth from cate page
     # returns auth for timetable
     def get_enrolled_class(self, cate_page):
         timetable_key = self._get_value_by_name(cate_page, 'keyt')
@@ -39,11 +40,11 @@ class CateSubmission:
 
     def fetch_timetable(self, timetable_key, timetable_class, timetable_period):
         payload = {
-        'keyt':timetable_key, 
-        'class':timetable_class, 
+        'keyt':timetable_key,
+        'class':timetable_class,
         'period':timetable_period
         }
-        
+
         r = requests.get(self.base_url + 'timetable.cgi', auth=self.auth, params=payload)
         return BeautifulSoup(r.text)
 
@@ -61,9 +62,9 @@ class CateSubmission:
 
     # returns the url of the homework page
     def get_submission_url(self, handin_urls):
-        print('Which [assignment] would you like to hand in?')
+        print 'Which [assignment] would you like to hand in?'
         for i, key in enumerate(handin_urls.keys()):
-            print('[{}]\t{}'.format(i, key))
+            print '[{}]\t{}'.format(i, key)
 
         invalid_key_selected = True
         selected_key = None
@@ -73,116 +74,138 @@ class CateSubmission:
 
         return handin_urls[handin_urls.keys()[int(selected_key)]]
 
-    def fetch_submission_page(submission_url):
+    def fetch_submission_page(self, submission_url):
         r = requests.get(self.base_url + submission_url, auth=self.auth)
         return r.text
 
-    def submit_assignment(self, submission_page):
+    def submit_assignment(self, submission_page, url):
         if 'Group' in submission_page:
             if 'No declaration' in submission_page:
-                print('Do you want to create a group on CATE and submit the work')
+                print 'Do you want to create a group on CATE and submit the work'
                 user_response = raw_input('Do you want to create one? [Y/n]: ')
                 if 'Y' in user_response:
-                    submission_page = _submit_declaration()
-                    _add_members_to_group()
-                    _submit_files(submission_url, _get_files())
+                    submission_page = self._submit_declaration(submission_page)
+                    self._add_members_to_group()
+                    file_keys = self._get_file_keys(submission_page)
+                    self._submit_files(url, self._get_files(file_keys))
                 else:
-                    print('Come back to sign the declaration once a group has been formed')
+                    print 'Come back to sign the declaration once a group has been formed'
                     exit()
             else:
-                _submit_declaration()
-                print('The declaration has been signed')
+                self._submit_declaration(submission_page)
+                print 'The declaration has been signed'
                 exit()
         else:
-            submission_page = _submit_declaration()
-            _submit_files(submission_url, _get_files())
+            submission_page = self._submit_declaration(submission_page)
+            self._get_file_keys(submission_page)
+            file_keys = self._get_file_keys(submission_page)
+            self._submit_files(url, self._get_files(file_keys))
+
+    def _get_file_keys(self, submission_page):
+        files = {}
+        if os.path.isdir('.git'):
+            self._create_cate_token()
+            name = 'cate_token'
+            files[name] = submission_page.text.find(name)
+
+        for arg in sys.argv[1:]:
+            name = arg.split('.')[0]
+            files[name] = submission_page.text.find(name)
+
+        files = collections.OrderedDict(sorted(files.items()))
+        file_keys = {}
+        for i, file_key in enumerate(page.find_all(type='file')):
+            file_keys[file_key['name']] = files.keys()[i]
+
+        return file_keys
+
+    def _get_files(self, file_keys):
+        files = {}
+        for i, name in enumerate(file_keys.values()):
+            for file_path in sys.argv[1:]: 
+                print 'is {} in {}'.format(name, file_path)
+                if name in file_path:
+                    files[file_keys.keys()[i]] = self._attach_file(file_path)
+        return files
+    
+    def _attach_file(self, file_path):
+        try:
+            f = open(file_path, 'rb')
+            print '{} added'.format(file_path)
+        except IOError:
+            print 'Fatal: IOError - file does not exist'
+            exit()
+
+        return f
 
     # adds memebers to the group
     def _add_members_to_group(self, submission_page):
-        self._init_students(page)
-        readline.set_completer(self.completer)
+        self._init_students(submission_page)
+        readline.set_completer(self._completer)
         readline.set_completer_delims('')
         readline.parse_and_bind("tab: complete")
 
         student_name = ''
-        while ('DONE' not in student_name):
+        while 'DONE' not in student_name:
             if student_name in self.students.keys():
                 student = self.students[student_name]
-                print'{} has been added to group'.format(student)
+                print '{} has been added to group'.format(student)
             else:
-                print 'Use TAB to auto-complete or DONE when finished'
-            student_name = raw_input('Enter Surname, Firstname.    E.G Joshi, Preeya (hj1612) - c2-0\n')
+                print 'Enter Surname, Firstname.    E.G Joshi, Preeya (hj1612) - c2-0'
+            student_name = raw_input('Use TAB to auto-complete or DONE when finished\n')
 
-    def completer(self, text, state):
+    def _completer(self, text, state):
         options = [x for x in self.students.keys() if x.startswith(text)]
         try:
             return options[state]
         except IndexError:
             return None
-    
+
     def _init_students(self, submission_page):
         list_of_students = submission_page.find_all('option')
         self.students = dict((student.get_text(), student['value']) for student in list_of_students)
 
-    def _get_files(self):
-        files = None
-        if (os.path.isdir('.git')):
-            _create_cate_token()
-            _attach_file('cate_token.txt', files)
-        for arg in sys.argv[1:]
-            _attach_file(str(arg), files)
-        return files
-
-    def _attach_file(self, file_path, files):
-        try:
-            # HERE ERROR - 195 NOT ALWAYS CORRECT  
-            files['file-195-none'] = open(file_path, 'rb')}
-            print '{} created'.format(file_path)
-        except IOError:
-            print 'Fatal: IOError - file does not exist'
-            exit()
-
-        return files
-
     def _create_cate_token(self):
-        call = subprocess.call("git rev-parse HEAD > cate_token.txt", shell=True)
+        subprocess.call("git rev-parse HEAD > cate_token.txt", shell=True)
         print 'cate_token.txt created'
 
     def _submit_files(self, submission_url, files, auth):
-        submit_key = _get_value_by_name('key').split(':')
+        submit_key = self._get_value_by_name('key').split(':')
         submit_key = ':'.join(submit_key[:4] + ['submit',] + submit_key[5:])
         payload={'key':submit_key}
         r = requests.post(submission_url, files=files, data=payload, auth=auth)
 
         if 'NOT SUBMITTED' in r.text:
-            print('File failed to upload, check extension or base name')
+            print 'Files failed to upload, check extension or base name'
         elif r.status_code == 200:
-            print('Boom! You\'re done')
+            print 'Files submitted.'
+            print 'Boom! You\'re done'
         else:
-            print('Something went wrong, try again or submit on CATE')
+            print 'Something went wrong, try again or submit on CATE'
             exit()
 
-    def _submit_declaration(self):
-        payload = { 
-        'inLeader':_get_value_by_name('inLeader'), 
-        'inMember':_get_value_by_name('inMember'), 
-        'version':_get_value_by_name('version'),
-        'key':_get_value_by_name('key')
+    def _submit_declaration(self, submission_page):
+        payload = {
+            'inLeader': self._get_value_by_name('inLeader'),
+            'inMember': self._get_value_by_name('inMember'),
+            'version': self._get_value_by_name('version'),
+            'key': self._get_value_by_name('key')
         }
-        declartion_url = soup.find('form')['action']
+        declartion_url = submission_page.find('form')['action']
 
         return BeautifulSoup(requests.post(self.base_url + declartion_url, data=payload, auth=self.auth))
 
     def _get_value_by_name(self, soup, name, extra_attrs={}):
-        attrs = {'name':name}
+        attrs = {'name': name}
         attrs.update(extra_attrs)
         return soup.find('input', attrs=attrs)['value']
+
 
 def raw(message):
     return raw_input(message)
 
+
 def main():
-    # handle command line args
     cate = CateSubmission(lambda: raw('Username: '), lambda: getpass.getpass('Password: '))
 
     cate_page = cate.fetch_cate_homepage()
@@ -194,8 +217,8 @@ def main():
     submission_url = cate.get_submission_url(assignments)
     submission_page = cate.fetch_submission_page(submission_url)
 
-    cate.submit_assignment(submission_page)
+    cate.submit_assignment(submission_page, submission_url)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
